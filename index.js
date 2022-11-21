1fa5e7371b1fb6618aa8de6ccb6d539a788b3539
Tyler <44725452+changzhenlin@users.noreply.github.com>
2022-11-21 16:28:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,24 @@ function generateProgressBar() {
 
 const readme = `\
 
+### Hi there 👋
+
+
+Here are some ideas to get you started:
+
+- 😄 I’m a developer and shutterbug
+- 🔭 I’m currently working on a tech company in Beijing
+- 🌱 I’m currently learning Typescript
+- 🤔 I’m looking for help with my photo and my code
+- 💬 Ask me about everything
+- 📫 How to reach me: chang_zhenlin@126.com
+- ⚡ Fun fact: This page mustn't get more than 1k views🤣
+
+![Snake animation](https://github.com/changzhenlin/changzhenlin/blob/output/github-contribution-grid-snake.svg)
+
+<!--START_SECTION:waka-->
+<!--END_SECTION:waka-->
+
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
 ---
