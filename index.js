5d7f555b8bbfa66356b6fa85b1c3732f6614d117
Tyler <44725452+changzhenlin@users.noreply.github.com>
2022-11-21 18:47:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ Here are some ideas to get you started:
 
 ![Github Stats](https://github-readme-stats.vercel.app/api?username=changzhenlin&show_icons=true&theme=dark&count_private=true)
 
+![Most Used Languages](https://github-readme-stats.vercel.app/api/top-langs/?username=coderfix-lab&theme=dark&layout=compact)
+
 <!--START_SECTION:waka-->
 <!--END_SECTION:waka-->
 
