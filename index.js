1d44c7f483384a91694ed1854c1208a4ef6a81f2
Tyler <44725452+changzhenlin@users.noreply.github.com>
2022-11-21 16:23:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,12 @@ function generateProgressBar() {
 }
 
 const readme = `\
-### Hi there 👋
+
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
+
 ---
+
 ⏰ Updated on ${new Date().toUTCString()}
----
 `
+
+console.log(readme)
